653d52fc623acf4a85c43810ad6b690f04ad4a9a
Jayanti Pesa <jayanti.pesa@gmail.com>
2020-09-13 21:14:18 +0530

    Added profile page displaying all articles written by user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def show
+        @user = User.find(params[:id])
+    end
+
     def edit
         @user = User.find(params[:id])
     end
@@ -31,6 +35,5 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
-
 
 end
